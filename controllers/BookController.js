81f87296e75f11ba855cf4b4883055cf4104aca9
Kwazimodd <mrcross622@gmail.com>
2026-02-19 21:11:39 +0200

    New Commit 4

--- controllers/BookController.js
+++ controllers/BookController.js
@@ -5,5 +5,6 @@ module.exports = class BookController {
     async findBookById(bookId) {
         const response = await axios.get('https://demoqa.com/books', { params: { search: bookId } })
         expect(response.status).toEqual(200);
+        console.log(response.data);
     }
 }
